ec95dfbdad4f3012f408f3959eda73e0378a46b1
Priya Choudhary <priya@tothenew.com>
2016-04-30 21:44:42 +0530

    import and export example

---
+++ examples/demo_export.js
@@ -0,0 +1,10 @@
+/**
+ * Created by priya on 27/4/16.
+ */
+//export * from "demo" not working
+
+export function sum (x, y) { return x + y };
+export var pi = 3.141593;
+export default function(){
+  console.log("this is a default module");
+}
